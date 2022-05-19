94a4e8e512b6730257d76bfee9064833242f8130
Dimitris Kapanidis <d.kapanidis@gmail.com>
2022-05-19 11:57:55 +0200

    update images

--- components/features/BirdsEye.tsx
+++ components/features/BirdsEye.tsx
@@ -3,10 +3,10 @@ import React from "react";
 
 export default function BirdsEye() {
   return (
-    <FeatureSection title="Bird's eye view of the Cluster state" img="/images/deployment-nginx.png" alt="deployment process of nginx">
+    <FeatureSection title="Bird's eye view of the Cluster state" img="/images/deployment-nginx.gif" alt="deployment process of nginx">
       <li>One screen to view all the related cluster&apos;s objects and their dependencies</li>
       <li>Red/Green ready-state for all objects to provide a quick health check view</li>
-      <li>Quick action buttons to facilitate deletion or scaling of the application</li>
+      <li>Quick actions to facilitate deletion or scaling of the application</li>
     </FeatureSection>
   )
 }
