d04eb12decd6b5b4bc1b21d3f6f021d2b0b6b243
Dimitris Kapanidis <d.kapanidis@gmail.com>
2022-05-31 12:37:56 +0200

    remove deprecated reveal lib

--- components/FeatureSection.tsx
+++ components/FeatureSection.tsx
@@ -1,6 +1,5 @@
 import { keyframes } from "@emotion/react";
 import Image from "next/image";
-import Reveal from "react-awesome-reveal";
 
 const customAnimation = keyframes`
   from {
@@ -18,28 +17,26 @@ type FeatureSectionProps = { title: string, img: string, alt: string, children: 
 export default function FeatureSection({ title, img, alt, children }: FeatureSectionProps) {
   return (
     <div className="py-2 bg-white">
-      <Reveal triggerOnce={true} keyframes={customAnimation}>
-        <div className="flex-row lg:flex">
-          <div className="relative px-4 py-6 overflow-hidden sm:px-6 sm:py-8 lg:p-8 xl:p-16">
-            <h3 className="mt-2 text-3xl leading-8 font-bold tracking-tight text-blue-900 sm:text-4xl sm:leading-10">
-              {title}
-            </h3>
-            <ul className="pt-5 list-disc pl-4 leading-8 italic">
-              {children}
-            </ul>
-          </div>
-          <div className="py-16 px-4">
-            <Image
-              src={img}
-              alt={alt}
-              className="shadow-2xl"
-              width={800}
-              height={500}
-              layout="intrinsic"
-            />
-          </div>
+      <div className="flex-row lg:flex">
+        <div className="relative px-4 py-6 overflow-hidden sm:px-6 sm:py-8 lg:p-8 xl:p-16">
+          <h3 className="mt-2 text-3xl leading-8 font-bold tracking-tight text-blue-900 sm:text-4xl sm:leading-10">
+            {title}
+          </h3>
+          <ul className="pt-5 list-disc pl-4 leading-8 italic">
+            {children}
+          </ul>
         </div>
-      </Reveal>
+        <div className="py-16 px-4">
+          <Image
+            src={img}
+            alt={alt}
+            className="shadow-2xl"
+            width={800}
+            height={500}
+            layout="intrinsic"
+          />
+        </div>
+      </div>
     </div>
   )
 }
@@ -48,26 +45,24 @@ type FeatureBlackSectionProps = { title: string, img: string, alt: string, child
 export function FeatureBlackSection({ title, img, alt, children }: FeatureBlackSectionProps) {
   return (
     <div className="py-2 bg-gray-800 signal background">
-      <Reveal triggerOnce={true} keyframes={customAnimation}>
       <div className="flex-row-reverse lg:flex">
-          <div className="relative px-4 py-6 overflow-hidden sm:px-6 sm:py-8 lg:p-8 xl:p-16">
-            <h3 className="mt-2 text-3xl leading-8 font-bold tracking-tight text-gray-200 sm:text-4xl sm:leading-10">{title}</h3>
-            <ul className="pt-5 list-disc pl-4 leading-8 italic text-gray-400">
-              {children}
-            </ul>
-          </div>
-          <div className="py-16 px-4">
-            <Image
-              src={img}
-              alt={alt}
-              className="shadow-2xl"
-              width={800}
-              height={500}
-              layout="intrinsic"
-              />
-          </div>
+        <div className="relative px-4 py-6 overflow-hidden sm:px-6 sm:py-8 lg:p-8 xl:p-16">
+          <h3 className="mt-2 text-3xl leading-8 font-bold tracking-tight text-gray-200 sm:text-4xl sm:leading-10">{title}</h3>
+          <ul className="pt-5 list-disc pl-4 leading-8 italic text-gray-400">
+            {children}
+          </ul>
         </div>
-      </Reveal>
+        <div className="py-16 px-4">
+          <Image
+            src={img}
+            alt={alt}
+            className="shadow-2xl"
+            width={800}
+            height={500}
+            layout="intrinsic"
+          />
+        </div>
+      </div>
     </div>
   )
 }
