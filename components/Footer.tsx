8d62f09f2def907f077aa7904efb4f122689c439
spiddy <dimitris.kapanidis@harbur.io>
2022-08-07 10:19:45 +0200

    fix footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import { HeartIcon } from "./Icons"
 
 export default function Footer() {
   return (
-    <div className="border-t border-gray-300 border-dashed p-8 pb-6 md:pb-10">
+    <div className="flex flex-col border-t border-gray-300 border-dashed p-8 pb-6 md:pb-10 gap-4">
       <div className="text-center italic text-xs text-gray-700">
         <div className="inline-flex">©&nbsp;<a rel="noreferrer" target="_blank" className="dotted" href="https://harbur.io">Harbur Cloud Solutions</a>, 2022. Made with <HeartIcon />
         </div>
