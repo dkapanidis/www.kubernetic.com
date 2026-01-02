921ba16c392a80ac205fe4b758c6eeba9057c765
Dimitris Kapanidis <d.kapanidis@gmail.com>
2026-01-02 17:05:13 -0600

    update copyright

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <div className="flex flex-col border-t border-gray-300 border-dashed p-8 pb-6 md:pb-10 gap-4">
       <div className="text-center italic text-xs text-gray-700">
-        <div className="inline-flex">©&nbsp;<a rel="noreferrer" target="_blank" className="dotted" href="https://harbur.io">Harbur Cloud Solutions</a>, 2022. Made with <HeartIcon />
+        <div className="inline-flex">©&nbsp;<a rel="noreferrer" target="_blank" className="dotted" href="https://harbur.io">Harbur Cloud Solutions</a>, 2026. Made with <HeartIcon />
         </div>
       </div>
       <div className="text-center italic text-xs text-gray-700">
