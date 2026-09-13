1fb91dc092a1fb8df2e6480a5d0046a99a331ae7
Dimitris Kapanidis <d.kapanidis@gmail.com>
2026-09-13 11:25:24 +0300

    fix scrolling

--- components/features/NativeKubernetes.tsx
+++ components/features/NativeKubernetes.tsx
@@ -8,10 +8,10 @@ export default function NativeKubernetes() {
           <h3 className="mt-2 text-3xl leading-8 font-extralight tracking-widest text-gray-200 sm:text-4xl sm:leading-10">
             Native Kubernetes support
           </h3>
-          <ul className="flex pt-8">
-            <li className="flex-1 text-gray-100 text-center text-lg px-4 py-2 m-2">Complete replacement of CLI</li>
-            <li className="flex-1 text-gray-100 text-center text-lg px-4 py-2 m-2">No need to memorize all commands</li>
-            <li className="flex-1 text-gray-100 text-center text-lg px-4 py-2 m-2">x10 faster than using CLI</li>
+          <ul className="flex flex-col sm:flex-row pt-8">
+            <li className="flex-1 min-w-0 text-gray-100 text-center text-lg px-4 py-2 m-2">Complete replacement of CLI</li>
+            <li className="flex-1 min-w-0 text-gray-100 text-center text-lg px-4 py-2 m-2">No need to memorize all commands</li>
+            <li className="flex-1 min-w-0 text-gray-100 text-center text-lg px-4 py-2 m-2">x10 faster than using CLI</li>
           </ul>
         </div>
       </div>
